5bee01d06173dae9c94c73fe3f992802b806c576
惠思雨 <huisiyu@bimwinner.com>
2021-06-04 16:47:42 +0800

    prettier配置规则的更改

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,8 @@ module.exports = getESLintConfig('react-ts', {
     '@typescript-eslint/explicit-function-return-type': 0,
     'arrow-parens': 0,
     '@typescript-eslint/no-invalid-void-type': 0,
+    '@typescript-eslint/no-shadow': 0,
+    'function-paren-newline': 0,
+    'no-confusing-arrow': 0,
   },
 });
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,5 @@
-/** @format */
-
-module.exports = {
+const { getPrettierConfig } = require('@iceworks/spec');
+module.exports = getPrettierConfig('react', {
   printWidth: 120, // 每行的宽度
   semi: true, // 语句行尾是否添加分号
   singleQuote: true, // 字符串使用单引号
@@ -8,5 +7,4 @@ module.exports = {
   bracketSpacing: true, // 对象字面量的大括号内部是否添加空格
   jsxBracketSameLine: true, // 是否将>放置在多行 JSX 元素最后一行的结尾，而不是放在下一行
   arrowParens: 'avoid', // 箭头函数只有一个参数时，参数是否使用圆括号
-  insertPragma: true,
-};
+});
